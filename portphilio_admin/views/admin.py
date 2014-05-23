b9b9a3f5cbfc9e2a689471899df430b495839c5c
Dustin Ingram <dustin.ingram@gmail.com>
2014-05-23 13:17:14 -0400

    Added rebuild endpoint

--- portphilio_admin/views/admin.py
+++ portphilio_admin/views/admin.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, send_file, abort, send_from_directory
 from flask import current_app as app
 from flask import render_template
 from flask.ext.login import login_required
+from portphilio_lib.build_db import build_db
 
 import os
 
@@ -11,3 +12,8 @@ mod = Blueprint('admin', __name__, static_folder='static', template_folder='temp
 @login_required
 def index():
     return render_template('index.html')
+
+@mod.route('/build_db')
+def rebuild():
+    build_db()
+    return "Success"
