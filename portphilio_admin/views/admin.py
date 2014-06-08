d8da6526e29859907a1af685be28fbed59d62390
Dustin Ingram <dustin.ingram@gmail.com>
2014-06-08 12:09:20 -0400

    Added image endpoint to admin

--- portphilio_admin/views/admin.py
+++ portphilio_admin/views/admin.py
@@ -3,6 +3,9 @@ from flask import current_app as app
 from flask import render_template
 from flask.ext.login import login_required
 from portphilio_lib.build_db import build_db
+from portphilio_lib.tools import retrieve_image
+from flask.ext.login import login_required
+from flask.ext.login import current_user
 
 import os
 
@@ -17,3 +20,7 @@ def index():
 def rebuild():
     build_db()
     return "Success"
+
+@mod.route('/image/<image_name>')
+def image(image_name):
+    return retrieve_image(image_name, current_user.username)
