081d9e9cc450fcd91e9207e1f3d283be8b3497cc
ryanhinkel <ryanhinkel@gmail.com>
2014-06-18 13:07:13 -0400

    sage class off for memory evaluation	modified:   portphilio_admin/views/static/js/admin_backbone_messages.js

--- portphilio_admin/views/static/js/admin_backbone_messages.js
+++ portphilio_admin/views/static/js/admin_backbone_messages.js
@@ -1,5 +1,6 @@
 var msg = (function(thisObj){
   var Messager = {
+    on: false,
     thread: null,
     threads: {
       'lookup':{
@@ -32,90 +33,92 @@ var msg = (function(thisObj){
     init: function(){
       var body = $('body');
 
-      var menu = Messager.menu = $('<div></div>')
-        .css({
-          'position':'fixed',
-          'bottom':'0',
-          'right': '0',
-          'padding':'0',
-          'width': '100%',
-          'z-index': '1001'
-        })
-        .appendTo(body);
+      if(Messager.on){
+        var menu = Messager.menu = $('<div></div>')
+          .css({
+            'position':'fixed',
+            'bottom':'0',
+            'right': '0',
+            'padding':'0',
+            'width': '100%',
+            'z-index': '1001'
+          })
+          .appendTo(body);
 
-      var display = Messager.display = $('<div></div>')
-        .addClass('msg_display')
-        .css({
-          'position':'fixed',
-          'top':'60%',
-          'left':'0',
-          'bottom':'0',
-          'right': '0',
-          'z-index': '1000'
-        })
-        .appendTo(body)
-        .hide();
+        var display = Messager.display = $('<div></div>')
+          .addClass('msg_display')
+          .css({
+            'position':'fixed',
+            'top':'60%',
+            'left':'0',
+            'bottom':'0',
+            'right': '0',
+            'z-index': '1000'
+          })
+          .appendTo(body)
+          .hide();
 
-      var displayList = Messager.displayList = $('<div></div>')
-        .css({
-          'padding':'1.5% 1.5% 3%'
-        })
-        .appendTo(display);
+        var displayList = Messager.displayList = $('<div></div>')
+          .css({
+            'padding':'1.5% 1.5% 3%'
+          })
+          .appendTo(display);
 
-      var displayMenu = $('<div></div>')
-        .css({
-          'position':'fixed',
-          'top': '60%',
-          'right': '0',
-          'width':'10%',
-          'text-align':'center'
-        })
-        .appendTo(display);
+        var displayMenu = $('<div></div>')
+          .css({
+            'position':'fixed',
+            'top': '60%',
+            'right': '0',
+            'width':'10%',
+            'text-align':'center'
+          })
+          .appendTo(display);
 
 
-      var close = $('<div class="close">X</div>')
-        .css({
-          'cursor':'pointer',
-          'font-size':'50px',
-          'padding':'30%',
-          'margin':'20% 0'
-        })
-        .appendTo(displayMenu)
-        .on('click', function(){
-          display.fadeOut(100);
-        })
-
-      var clear = Messager.clear = $('<div class="clear">clear</div>')
-        .css({
-          'cursor':'pointer',
-          'font-size':'10px',
-          'padding':'5%',
-          'margin':'20% 0'
-        })
-        .appendTo(displayMenu)
-        .on('click', function(){
-          Messager.thread.messages = []
-          Messager.refreshDisplay();
-        })
-
-      _.each(Messager.threads, function(val, key){
-        $('<a>'+val.label+'</a>')
+        var close = $('<div class="close">X</div>')
           .css({
             'cursor':'pointer',
-            'background-color':'#ccc',
-            'display':'block',
-            'float':'right',
-            'padding':'.5%',
-            'margin':'1.5% 1.5% 0 0'
+            'font-size':'50px',
+            'padding':'30%',
+            'margin':'20% 0'
           })
-          .appendTo(menu)
+          .appendTo(displayMenu)
           .on('click', function(){
-            Messager.thread = val;
-            display.show();
+            display.fadeOut(100);
+          })
+
+        var clear = Messager.clear = $('<div class="clear">clear</div>')
+          .css({
+            'cursor':'pointer',
+            'font-size':'10px',
+            'padding':'5%',
+            'margin':'20% 0'
+          })
+          .appendTo(displayMenu)
+          .on('click', function(){
+            Messager.thread.messages = []
             Messager.refreshDisplay();
-            Messager.clear.html('clear '+ val.label);
-          });
-      }, Messager)
+          })
+
+        _.each(Messager.threads, function(val, key){
+          $('<a>'+val.label+'</a>')
+            .css({
+              'cursor':'pointer',
+              'background-color':'#ccc',
+              'display':'block',
+              'float':'right',
+              'padding':'.5%',
+              'margin':'1.5% 1.5% 0 0'
+            })
+            .appendTo(menu)
+            .on('click', function(){
+              Messager.thread = val;
+              display.show();
+              Messager.refreshDisplay();
+              Messager.clear.html('clear '+ val.label);
+            });
+        }, Messager)
+      }
     },
 
     cleanErrStack: function(err){
@@ -128,17 +131,19 @@ var msg = (function(thisObj){
     },
 
     log: function(msg, t){
-      var thread = Messager.threads[t] || Messager.threads['default'];
-      if(thread.on){
-        err = (new Error);
-        var stack = Messager.cleanErrStack(err);
-        var line = "<div class='msg'><a>" + msg + "</a>" + "<div class='stack'>" + stack + "</div></div>";
-        thread.messages.push(line);
-        if(Messager.thread === thread){
-          Messager.addToDisplay(line);
-        }
-        if(thread.console){
-          console.log(msg);
+      if(Messager.on){
+        var thread = Messager.threads[t] || Messager.threads['default'];
+        if(thread.on){
+          err = (new Error);
+          var stack = Messager.cleanErrStack(err);
+          var line = "<div class='msg'><a>" + msg + "</a>" + "<div class='stack'>" + stack + "</div></div>";
+          thread.messages.push(line);
+          if(Messager.thread === thread){
+            Messager.addToDisplay(line);
+          }
+          if(thread.console){
+            console.log(msg);
+          }
         }
       }
     },
