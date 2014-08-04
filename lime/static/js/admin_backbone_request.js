4e4a00daea7c9b31b211534b76726cfc4b46b54f
ryanhinkel <ryanhinkel@gmail.com>
2014-08-04 16:38:15 -0400

    Refactor to isolate API functions

--- lime/static/js/admin_backbone_request.js
+++ lime/static/js/admin_backbone_request.js
@@ -10,7 +10,144 @@
 // on success, remove them
 // on error, rerun once and then flag the manager to halt requests
 
+/* ------------------------------------------------------------------- */
+// Request API
+/* ------------------------------------------------------------------- */
 
+// Passed to the make request function to initiate
+// Can be called within themselves
+// granularity for requests
+
+// These functions will be called in the context of the request
+
+// these functions set up more requests
+// subrequests if many - callParallel/callSerial
+// request if only one - call
+
+
+App.RequestApi = {
+  batchPhotoUploadRequest: function(files, newPhotoNesting, model, predecessor){
+    var subrequests = this.subrequests = [];
+    // var request;
+
+    // if model is new, add to predecessor
+    if(model.isNew()){
+      vertices = [model];
+      edges = [[predecessor, model]];
+
+      subrequests.push(
+        App.requestPanel.initRequest('graphRequest',[vertices, edges])
+      );
+    }
+
+    // registering creates a reference to the parent of the subrequest
+    // also attached handler to request to listen to subrequest complete
+    this.registerSubrequests(subrequests);
+
+    // Call in Parallel
+    this.callParallelSubrequests(subrequests);
+
+    /* OLD CODE from before requestPanel refactor
+    // initiate new subrequests
+    var subrequests = _.map(files, function(file){
+      return this.photoUploadRequest(file, newPhotoNesting, model);
+    }, this);
+
+    // register subrequests
+    request.registerSubrequests(subrequests);
+
+    // upload
+    // request.batchView = new App.Upload.batchProgressView({'className': 'batch'});
+    // App.actionPanel.$el.prepend(request.batchView.render().el);
+    */
+
+  },
+
+  photoUploadRequest: function(file, nesting, model){
+    var request = this.initRequest();
+    request.requestType = 'nestedphoto';
+    request.file = file;
+    request.nesting = nesting;
+    request.model = model;
+
+    var time = (Math.random() * 2000) + 1000;
+    setTimeout(function(){
+      request.trigger('complete', request);
+    }, time)
+
+    return request;
+  },
+
+  graphRequest: function(vertices, edges){
+    var subrequests = this.subrequests = [
+      App.requestPanel.initRequest('verticesRequest',[vertices]),
+      App.requestPanel.initRequest('edgesRequest',[edges])
+    ];
+
+    this.registerSubrequests(subrequests);
+    this.callSerialSubrequests(subrequests);
+  },
+
+  verticesRequest: function(vertices){
+    // test
+    // this.trigger('complete', this);
+    // return false;
+
+    console.log(vertices);
+
+    var subrequests = _.map(vertices, function(vertex){
+      return App.requestPanel.initRequest('vertexRequest',[vertex]);
+    }, this);
+
+    console.log(subrequests.length);
+
+    this.registerSubrequests(subrequests);
+    this.listenTo(this, 'subrequestscomplete', function(){
+      console.log('Vertices Done');
+    })
+    this.callParallelSubrequests(subrequests);
+
+  },
+
+  vertexRequest: function(vertex){
+    var _cls = vertex.get('_cls')
+
+    // client side
+    var collection = App.collection[_cls];
+    collection.add(vertex);
+
+    // persistence
+    vertex.save(vertex.changedAttributes(), {'success':this.complete,'error':this.error});
+  },
+
+  edgesRequest: function(edges){
+
+    console.log(edges);
+    var subrequests = _.map(edges, function(edge){
+      return App.requestPanel.initRequest('edgeRequest',[edge]);
+    }, this);
+
+    this.registerSubrequests(subrequests);
+
+    this.listenTo(this, 'subrequestscomplete', function(){
+      console.log('Edges done');
+    })
+    this.callParallelSubrequests(subrequests);
+
+  },
+
+  edgeRequest: function(edge){
+    var vertex = edge[0];
+    console.log(vertex);
+    vertex.addToSuccset(edge[1], {'success':this.complete,'error':this.error});
+
+    console.log('added '+ vertex.get('_cls') + ' to ' + edge[1].get('_cls'));
+  }
+}
+
+/* ------------------------------------------------------------------- */
+// Request Function
+/* ------------------------------------------------------------------- */
 
 App.Request = Backbone.View.extend({
 
@@ -89,6 +226,10 @@ App.Request = Backbone.View.extend({
 
 });
 
+/* ------------------------------------------------------------------- */
+// Request Panel
+/* ------------------------------------------------------------------- */
+
 App.RequestPanel = Backbone.View.extend({
   el: $('#request_panel'),
   requestsMade: 0,
@@ -106,143 +247,6 @@ App.RequestPanel = Backbone.View.extend({
     return request;
   },
 
-  // Private subrequests
-  // granularity for requests
-
-  // These functions will be called in the context of the request
-  // They are groupings of smaller functions related to server requests
-  // This is where I could load any controller function
-
-  batchPhotoUploadRequest: function(files, newPhotoNesting, model, predecessor){
-    // by convention:
-    // subrequests if many - callParallel/callSerial
-    // request if only one - call
-    var subrequests = this.subrequests = [];
-    // var request;
-
-    // if model is new, add to predecessor
-    if(model.isNew()){
-      vertices = [model];
-      edges = [[predecessor, model]];
-
-      subrequests.push(
-        App.requestPanel.initRequest('graphRequest',[vertices, edges])
-      );
-    }
-
-    // registering creates a reference to the parent of the subrequest
-    // also attached handler to request to listen to subrequest complete
-    this.registerSubrequests(subrequests);
-
-    // Call in Parallel
-    this.callParallelSubrequests(subrequests);
-
-    /* OLD CODE from before requestPanel refactor
-    // initiate new subrequests
-    var subrequests = _.map(files, function(file){
-      return this.photoUploadRequest(file, newPhotoNesting, model);
-    }, this);
-
-    // register subrequests
-    request.registerSubrequests(subrequests);
-
-    // upload
-    // request.batchView = new App.Upload.batchProgressView({'className': 'batch'});
-    // App.actionPanel.$el.prepend(request.batchView.render().el);
-    */
-
-  },
-
-  photoUploadRequest: function(file, nesting, model){
-    var request = this.initRequest();
-    request.requestType = 'nestedphoto';
-    request.file = file;
-    request.nesting = nesting;
-    request.model = model;
-
-    var time = (Math.random() * 2000) + 1000;
-    setTimeout(function(){
-      request.trigger('complete', request);
-    }, time)
-
-    return request;
-
-  },
-
-  graphRequest: function(vertices, edges){
-    // this function sets up more requests
-    // by convention:
-    // subrequests if many - callParallel/callSerial
-    // request if only one - call
-
-    var subrequests = this.subrequests = [
-      App.requestPanel.initRequest('verticesRequest',[vertices]),
-      App.requestPanel.initRequest('edgesRequest',[edges])
-    ]
-
-    // Always register listeners before calling
-    //this.listenTo(vertReq, 'complete', null)
-
-    this.registerSubrequests(subrequests);
-    this.callSerialSubrequests(subrequests);
-  },
-
-  verticesRequest: function(vertices){
-    // test
-    // this.trigger('complete', this);
-    // return false;
-
-    console.log(vertices);
-
-    var subrequests = _.map(vertices, function(vertex){
-      return App.requestPanel.initRequest('vertexRequest',[vertex]);
-    }, this);
-
-    console.log(subrequests.length);
-
-    this.registerSubrequests(subrequests);
-    this.listenTo(this, 'subrequestscomplete', function(){
-      console.log('Vertices Done');
-    })
-    this.callParallelSubrequests(subrequests);
-
-  },
-
-  vertexRequest: function(vertex){
-    var _cls = vertex.get('_cls')
-
-    // client side
-    var collection = App.collection[_cls];
-    collection.add(vertex);
-
-    // persistence
-    vertex.save(vertex.changedAttributes(), {'success':this.complete,'error':this.error});
-  },
-
-  edgesRequest: function(edges){
-
-    console.log(edges);
-    var subrequests = _.map(edges, function(edge){
-      return App.requestPanel.initRequest('edgeRequest',[edge]);
-    }, this);
-
-    this.registerSubrequests(subrequests);
-
-    this.listenTo(this, 'subrequestscomplete', function(){
-      console.log('Edges done');
-    })
-    this.callParallelSubrequests(subrequests);
-
-  },
-
-  edgeRequest: function(edge){
-    var vertex = edge[0];
-    console.log(vertex);
-    vertex.addToSuccset(edge[1], {'success':this.complete,'error':this.error});
-
-    console.log('added '+ vertex.get('_cls') + ' to ' + edge[1].get('_cls'));
-  },
-
   getId: function(){
     return this.requestsMade++;
   },
@@ -254,7 +258,7 @@ App.RequestPanel = Backbone.View.extend({
 
     // create new request
     var options = options || {};
-    options.content = {'func': this[functionName],'args':arguments};
+    options.content = {'func': App.RequestApi[functionName],'args':arguments};
     options.type = functionName;
     options.rid = this.getId();
     var request = new App.Request(options);
