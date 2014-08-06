640e7506f0943d483e99676c067947604b906a24
Dustin Ingram <dustin.ingram@gmail.com>
2014-08-06 15:44:03 -0400

    Added newrelic reporting

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 import lime
+import newrelic.agent
 
 if __name__ == '__main__' :
     app = lime.app
+    newrelic.agent.initialize('newrelic.ini')
     port = int(os.environ.get('PORT', 5001))
     app.run(host="0.0.0.0", port=port, use_debugger=True, use_reloader=True)
