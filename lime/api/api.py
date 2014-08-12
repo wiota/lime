cb139918c840d20e5d575d7efaad8e49b26412e4
Dustin Ingram <dustin.ingram@gmail.com>
2014-08-12 17:01:20 -0400

    Handle missing fields in PUT for work and category

--- lime/api/api.py
+++ lime/api/api.py
@@ -78,12 +78,15 @@ def post_work():
 @login_required
 def put_work(id):
     doc = Work.objects.get(owner=current_user.id, id=id)
-    data = {k: request.json[k] for k in doc.get_save_fields()}
+
+    # TODO: This is a bad function
+    data = {k: request.json[k] for k in doc.get_save_fields() if k in request.json.keys()}
     update_document(doc, data).save()
 
     # TODO: This is a hack. get_save_fields should be reworked.
-    doc.reload()
-    doc.update(set__cover=request.json['cover'])
+    if 'cover' in request.json.keys():
+        doc.reload()
+        doc.update(set__cover=request.json['cover'])
 
     return jsonify(result="success"), 200  # TODO: Should be a 204
 
@@ -124,8 +127,16 @@ def post_category():
 @login_required
 def put_category(id):
     doc = Category.objects.get(owner=current_user.id, id=id)
-    data = {k: request.json[k] for k in doc.get_save_fields()}
+
+    # TODO: This is a bad function
+    data = {k: request.json[k] for k in doc.get_save_fields() if k in request.json.keys()}
     update_document(doc, data).save()
+
+    # TODO: This is a hack. get_save_fields should be reworked.
+    if 'cover' in request.json.keys():
+        doc.reload()
+        doc.update(set__cover=request.json['cover'])
+
     return jsonify(result="success"), 200  # TODO: Should be a 204
 
 
