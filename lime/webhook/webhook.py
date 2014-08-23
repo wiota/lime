bccaafca697cd3d8b8b069dcfca04e83f9b936fc
Dustin Ingram <dustin.ingram@gmail.com>
2014-08-23 02:16:43 -0400

    Add StripeEmail

--- lime/webhook/webhook.py
+++ lime/webhook/webhook.py
@@ -6,7 +6,7 @@ from flask import url_for
 from flask.ext.login import login_required
 from flask.ext.login import current_user
 from toolbox.models import User, Host
-from toolbox.email import *
+from toolbox.email import StripeEmail
 from time import strftime, localtime
 import stripe
 from toolbox.template_tools import format_date
@@ -16,7 +16,7 @@ mod = Blueprint('webhook', __name__, template_folder='views', url_prefix='/webho
 
 @mod.route('/stripe/', methods=['POST'])
 def stripe_hook():
-    print request.json
+    StripeEmail(request.json).send()
     stripe.api_key = app.config['STRIPE_SECRET_KEY']
     e = stripe.Event.retrieve(request.json["id"])
     if e["type"] == "invoice.created" :
